8c3fa0cb049d1cfe42b88e1397921a646c20ac2c
dipeshsocials <dipeshsocials@gmail.com>
2026-07-01 21:26:32 +0530

    Complete Python basics: data types, user input and operators

---
+++ 01_basics/data_types.py
@@ -0,0 +1,36 @@
+# String
+name = "Dev"
+
+# Integer
+age = 21
+
+# Float
+height = 1.78
+
+# Boolean
+is_student = True
+
+# List
+languages = ["Python", "C", "Java"]
+
+# Tuple
+marks = (90, 95, 88)
+
+# Dictionary
+student = {
+    "name": "Dev",
+    "age": 21,
+    "city": "Baraut"
+}
+
+# Set
+numbers = {1, 2, 3, 4, 5}
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(languages))
+print(type(marks))
+print(type(student))
+print(type(numbers))
